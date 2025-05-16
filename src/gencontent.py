bbf45e465e76a742afeb35f19d3453e1e76ffb8e
Bennett Robinson <brobinson9895@gmail.com>
2025-05-16 15:08:41 -0600

    add docs

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ from text_blocks_markdown import markdown_to_htmlnode, extract_title
 import shutil
 import os
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
     with open(from_path, "r", encoding="utf-8") as file:
         markdown = file.read()
@@ -11,27 +11,28 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path, "r", encoding="utf-8") as file:
         template_html = file.read()
     output_html = template_html.replace("{{ Title }}", title).replace("{{ Content }}", content_html)
+    output_html = output_html.replace("href=\"/", f"href={basepath}").replace("src=\"/", f"src={basepath}")
     with open(dest_path, "w") as file:
         file.write(output_html)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for file in os.listdir(dir_path_content):
         src_path = os.path.join(dir_path_content, file)
         dst_path = os.path.join(dest_dir_path, file)
         if os.path.isfile(src_path):
             dst_path = dst_path.replace(".md", ".html")
-            generate_page(src_path, template_path, dst_path)
+            generate_page(src_path, template_path, dst_path, basepath)
         elif os.path.isdir(src_path):
             os.mkdir(dst_path)
-            generate_page_recursive(src_path, template_path, dst_path)
+            generate_page_recursive(src_path, template_path, dst_path, basepath)
     
 
 def clean_public_dir():
-    if os.path.exists("public/"):
-        shutil.rmtree("public/")
-        os.mkdir("public")
+    if os.path.exists("docs/"):
+        shutil.rmtree("docs/")
+        os.mkdir("docs")
     else:
-        os.mkdir("public")
+        os.mkdir("docs")
 def copy_content_to_public(src_dirc, dst_dirc):
     if not os.path.exists(src_dirc):
         raise FileNotFoundError("this path does not exist")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 from gencontent import clean_public_dir, copy_content_to_public, generate_page_recursive
-
+import sys
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
 
 def main():
+    basepath = sys.argv[0] or '/'
+
     print("Deleting Public Directory")
     clean_public_dir()
 
@@ -14,7 +16,7 @@ def main():
     copy_content_to_public(dir_path_static, dir_path_public)
 
     print("Generating Content....")
-    generate_page_recursive(dir_path_content, template_path, dir_path_public)
+    generate_page_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
